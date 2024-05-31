cda2ee9e251fa84b89c23970da68a5686e18e856
zhanghaolong <zhanghaolong@megvii.com>
2024-05-31 22:16:42 +0800

    add more env check

--- toolchains/aarch64-linux-gnu.toolchain.cmake
+++ toolchains/aarch64-linux-gnu.toolchain.cmake
@@ -3,12 +3,12 @@ set(CMAKE_SYSTEM_PROCESSOR aarch64)
 
 # now we support cross build for aarch64-linux by clang
 # host always need install some base package, like clang, llvm, gcc, g++
-# and install sysroot package: libgcc-11-dev-arm64-cross and libstdc++-11-dev-arm64-cross
+# and install sysroot package: libgcc-11-dev-arm64-cross and libstdc++-11-dev-arm64-cross gcc-aarch64-linux-gnu
 # the version of 11 is depend on the version of gcc/g++ in host
 set(CMAKE_C_COMPILER clang)
 set(CMAKE_CXX_COMPILER clang++)
-set(CMAKE_C_FLAGS "--target=aarch64-linux-gnu" CACHE STRING "c compiler flags" FORCE)
-set(CMAKE_CXX_FLAGS "--target=aarch64-linux-gnu" CACHE STRING "cxx compiler flags" FORCE)
+set(CMAKE_CXX_COMPILER_TARGET aarch64-linux-gnu)
+set(CMAKE_C_COMPILER_TARGET aarch64-linux-gnu)
 set(CMAKE_COMMON_FLAG "-Wno-error=attributes -Wno-error=cpp -Wno-error=sign-compare")
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${CMAKE_COMMON_FLAG}")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${CMAKE_COMMON_FLAG}")
--- toolchains/arm-linux-gnueabihf.toolchain.cmake
+++ toolchains/arm-linux-gnueabihf.toolchain.cmake
@@ -3,12 +3,12 @@ set(CMAKE_SYSTEM_PROCESSOR aarch32)
 
 # now we support cross build for aarch64-linux by clang
 # host always need install some base package, like clang, llvm, gcc, g++
-# and install sysroot package: libgcc-11-dev-armhf-cross and libstdc++-11-dev-armhf-cross
+# and install sysroot package: libgcc-11-dev-armhf-cross and libstdc++-11-dev-armhf-cross gcc-arm-linux-gnueabi gcc-arm-linux-gnueabihf
 # the version of 11 is depend on the version of gcc/g++ in host
 set(CMAKE_C_COMPILER clang)
 set(CMAKE_CXX_COMPILER clang++)
-set(CMAKE_C_FLAGS "--target=arm-linux-gnueabihf" CACHE STRING "c compiler flags" FORCE)
-set(CMAKE_CXX_FLAGS "--target=arm-linux-gnueabihf" CACHE STRING "cxx compiler flags" FORCE)
+set(CMAKE_CXX_COMPILER_TARGET arm-linux-gnueabihf)
+set(CMAKE_C_COMPILER_TARGET arm-linux-gnueabihf)
 set(CMAKE_COMMON_FLAG "-Wno-error=attributes -Wno-error=cpp -Wno-error=sign-compare")
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${CMAKE_COMMON_FLAG}")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${CMAKE_COMMON_FLAG}")
